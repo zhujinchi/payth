e8d243448a5b7c886b60244949c0ee7a62f528bd
zhujinchi <37591371+zhujinchi@users.noreply.github.com>
2022-06-13 00:17:19 +0800

    update

---
+++ payth/lib/net/api.dart
@@ -0,0 +1,43 @@
+import 'dart:convert';
+
+import 'package:adaptive_dialog/adaptive_dialog.dart';
+import 'package:crypto/crypto.dart';
+import 'package:dio/dio.dart';
+
+class API {
+  samplePost() async {
+    // sample post
+    // String url = "http://1.14.103.90:5000/paypal_return";
+
+    // ///build Dio
+    // Dio dio = Dio();
+
+    // dio.options.headers['Authorization'] = SystemInfo.shared().token;
+
+    // ///build map
+    // Map<String, dynamic> map = {};
+
+    // map['deal_id'] = User.shared().deal_id;
+    // map['amount_USD'] = amount;
+
+    // ///post
+    // Response response = await dio.post(url, queryParameters: map);
+    // var data = response.data;
+    // print(data);
+
+    //sample get
+    // String url = "http://1.14.103.90:5000/paypal_wallet";
+
+    // ///build Dio
+    // Dio dio = Dio();
+
+    // dio.options.headers['Authorization'] = SystemInfo.shared().token;
+
+    // ///get
+    // Response response = await dio.get(url);
+
+    // var data = response.data;
+
+    // print(data);
+  }
+}
